fbd47845817865d68510d7cfa6cdf6bdc4db0de7
Bishesh <bishesh.sunam@gmail.com>
2021-03-29 00:33:23 +1300

    code refactor

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ window.addEventListener("load", (e) => {
 
 const changeText = () => {
   let text = document.getElementById("text");
-  text.innerHTML = "Design by Bishesh Sunam - 1537070";
+  text.innerHTML = "Bishesh Sunam - 1537070";
   text.classList.add("change__text");
 
   let banner = document.querySelector(".banner");
